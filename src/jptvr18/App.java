397a6bc49393a5aafdeb921bbe955758f2f2e6f9
pupil <pupil@ivkhk.ee>
2020-09-04 15:18:47 +0300

    PassManager

--- src/jptvr18/App.java
+++ src/jptvr18/App.java
@@ -5,12 +5,58 @@
  */
 package jptvr18;
 
+import entity.ResourseEntity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
  */
 class App{
+    private List<ResourcseEntity> ListPesourrces = new ArrayList<>();
     public void run() {
-        System.out.println("hello!");
+        System.out.println("--- Менеджер паролей!");
+        String repeat = "r";
+        do{System.out.println("Выберете задачу");
+        System.out.println("0 - выйти из программы");
+        System.out.println("1 - добавить ресурс");
+        System.out.println("2 - список  ресурсов");
+        Scanner scanner = new Scanner (System.in);
+        int task = scanner.nextInt();
+        switch (task){
+            
+            case 0:
+                repeat = "q";
+                break;
+            case 1:
+                System.out.println("Выберете задачу 1");
+                ResourseEntity resourse = new ResourseEntity ();
+                resourse.setName ("ivkhk");
+                resourse.setIrl  ("https://kutsehariduskeskus.ee/ru");
+                resourse.setLogin("admin");
+                resourse.setPassword ("123123");
+                break;
+            case 2:
+                System.out.println("Выберете задачу 2");
+                System.out.println("Список ресурсов:");
+                for(Resourse r: this.list)()
+                        System.out.printin(r.toString());
+                break;
+                
+            
+        }
+            
+        }while(!"q".equals(repeat));
+        
+                
+                
+        
+
+
+
+ 
+                       
     }
 }
